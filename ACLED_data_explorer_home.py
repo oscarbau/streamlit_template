62bcf0b0a2b63a4a6d831c7003475fccbb2180b1
ovbautistac@gmail.com <41731874+oscarbau@users.noreply.github.com>
2025-09-18 16:49:05 +0200

    Fixed home description

--- ACLED_data_explorer_home.py
+++ ACLED_data_explorer_home.py
@@ -36,9 +36,9 @@ st.markdown(
     (Armed Conflict Location & Event Data Project) API, 
     allowing users to select a country and time range to explore geolocated incidents such as 
     battles, violence against civilians, and protests. The app uses leafmap for dynamic mapping, 
-    pandas for data handling, and stores fetched results in to maintain interactivity across navigation. 
+    pandas for data handling, and stores fetched results to maintain interactivity across visualizations. 
     Country masks are loaded as GeoJSON files to define spatial boundaries, enabling contextual 
-    analysis. The app is designed for humanitarian, academic, or policy users needing timely insights 
+    analysis. The app is designed for humanitarian, academic, and policy users needing timely geospatial insights 
     into conflict dynamics.
     """
 )
